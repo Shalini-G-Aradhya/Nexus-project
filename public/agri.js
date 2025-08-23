2aa9e76ba7b9cd5280beea6325309e72292f7866
lakshmishree36 <lakshshree36@gmail.com>
2025-08-23 09:43:40 +0530

    buyers

---
+++ public/agri.js
@@ -0,0 +1,39 @@
+const crops = [
+  { name: "Tomato", price: "rs25/kg", location: "Bangalore" ,img:"tom.jpg"},
+  { name: "Wheat", price: "rs22/kg", location: "Mysore",img:"wheat.jpg" },
+  { name: "Onion", price: "rs30/kg", location: "Hubli" ,img:"oni.jpg"},
+   { name: "Rice", price: "rs50/kg", location: "raichur" ,img:"download.jpg"}
+];
+
+const cropList = document.getElementById("cropList");
+const ordersList = document.getElementById("ordersList");
+
+// Show crops
+crops.forEach(crop => {
+  let div = document.createElement("div");
+  div.className = "crop-card";
+  div.innerHTML = `
+    <img src="${crop.img}" alt="${crop.name}" width="150">
+    <h3>${crop.name}</h3>
+    <p>Price: ${crop.price}</p>
+    <p>Location: ${crop.location}</p>
+    <button onclick="buyCrop('${crop.name}', '${crop.price}')">Buy Now</button>
+    <button onclick="makeOffer('${crop.name}')">Make Offer</button>
+  `;
+  cropList.appendChild(div);
+});
+
+function buyCrop(name, price) {
+  let li = document.createElement("li");
+  li.textContent = `Ordered ${name} at ${price}`;
+  ordersList.appendChild(li);
+}
+
+function makeOffer(name) {
+  let offer = prompt("Enter your offer price for " + name);
+  if (offer) {
+    let li = document.createElement("li");
+    li.textContent = `Offer made for ${name} at ₹${offer}/kg`;
+    ordersList.appendChild(li);
+  }
+}
